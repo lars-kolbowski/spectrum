d3f2a1c2267a546ac96f17ff191c1c4965dd4780
lars-kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2016-02-02 18:11:24 +0100

    move labels dashed lines

--- src/graph/Peak.js
+++ src/graph/Peak.js
@@ -53,19 +53,6 @@ function Peak (data, graph){
 	this.tooltip = tooltip + " m/z: " + this.x + ", i: " + this.y
 
 	//svg elements
-	this.labelDrag = d3.behavior.drag();
-	this.labelDrag.on("drag", function() {
-			var coords = d3.mouse(this);
-			var frag = this.innerHTML;
-			for (var f = 0; f < self.fragments.length; f++){
-				if(self.fragments[f].name == frag){
-					self.labelHighlights[f].attr("x", coords[0]);
-					self.labels[f].attr("x", coords[0]);
-					self.labelHighlights[f].attr("y", coords[1]);
-					self.labels[f].attr("y", coords[1]);
-				}
-			}
-		});	
 	this.g = this.graph.peaks.append('g');
 	this.g.append("svg:title").text(this.tooltip);	// easy tooltip
 	if (this.fragments.length > 0) {
@@ -104,6 +91,35 @@ function Peak (data, graph){
 		}
 
 	  	//create frag labels
+	  	this.labelLine = self.g.append("line")
+	  			.attr("stroke-width", 1)
+				.attr("stroke", "Black")
+				.style("stroke-dasharray", ("3, 3"));	
+		this.labelDrag = d3.behavior.drag();
+		this.labelDrag.on("drag", function() {
+			var coords = d3.mouse(this);
+			var frag = this.innerHTML;
+			for (var f = 0; f < self.fragments.length; f++){
+				if(self.fragments[f].name == frag){
+					self.labelHighlights[f].attr("x", coords[0]);
+					self.labels[f].attr("x", coords[0]);
+					self.labelHighlights[f].attr("y", coords[1]);
+					self.labels[f].attr("y", coords[1]);
+				}
+			}
+			var startX = self.graph.x(self.x);
+			var startY = self.graph.y(self.y)
+			var mouseX = coords[0]-startX;
+			var mouseY = coords[1];
+
+			console.log("self.x:"+self.x+" graph.x():"+startX+" mouseX:"+mouseX);
+			if (Math.abs(startX - mouseX) > 10)
+				self.labelLine
+					.attr("x1", 0)
+					.attr("x2", mouseX)
+					.attr("y1", startY)
+					.attr("y2", mouseY);
+		});	
 		this.labels = []; // will be array of d3 selections
 		this.labelHighlights = []; // will be array of d3 selections
 		var fragCount = this.fragments.length;
