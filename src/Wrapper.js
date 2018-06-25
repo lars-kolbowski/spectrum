e9a8bb623ad4b5c4b6c397b3101c3aa4e2bcd9b6
Lars Kolbowski <lars.b.kolbowski@campus.tu-berlin.de>
2018-06-25 14:50:42 +0200

    fix closing span tags

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -53,7 +53,7 @@ xiSPEC.init = function(
 		+"	<div class='xispec_dynDiv_resizeDiv_br draggableCorner'></div>"
 		+"</div>"
 		+"<div id='xispec_spectrumControls'>"
-		+"<span id='xispec_extra_spectrumControls_before'><span>"
+		+"<span id='xispec_extra_spectrumControls_before'></span>"
 		+'<i class="xispec_btn xispec_btn-1a xispec_btn-topNav fa fa-download" aria-hidden="true" id="downloadSVG" title="download SVG" style="cursor: pointer;"></i>'
 		+"<label class='xispec_btn'>Move Labels<input id='moveLabels' type='checkbox'></label>"
 		+'<label class="xispec_btn" title="toggle measure mode on/off">Measure<input class="pointer" id="measuringTool" type="checkbox"></label>'
@@ -68,7 +68,7 @@ xiSPEC.init = function(
 		+'	<button id="reset" title="Reset to initial zoom level" class="xispec_btn xispec_btn-1 xispec_btn-1a">Reset Zoom</button>'
 		+'</form>'
 		+'<i id="toggleSettings" title="Show/Hide Settings" class="xispec_btn xispec_btn-1a xispec_btn-topNav fa fa-cog" aria-hidden="true"></i>'
-		+"<span id='xispec_extra_spectrumControls_after'><span>"
+		+"<span id='xispec_extra_spectrumControls_after'></span>"
 		+'<a href="http://spectrumviewer.org/help.php" target="_blank"><i title="Help" class="xispec_btn xispec_btn-1a xispec_btn-topNav fa fa-question" aria-hidden="true"></i></a>'
 		+"</div>"
 		+"</div>"
